c0f49888b5ed6a9c45da52020100a798397604fb
kun99 <kuns.km@gmail.com>
2023-10-24 17:49:48 +0700

    checks duration and creates and stores thumbnail in spaces

--- upload.py
+++ upload.py
@@ -4,8 +4,9 @@ from werkzeug.utils import secure_filename
 import boto3
 from moviepy.editor import VideoFileClip
 import tempfile
-import os
 import io
+import imageio
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -27,10 +28,14 @@ def upload():
             'description': request.form['desc']
         }
         if uploaded_file:
-            #temp file to use moviepy to check video duration
+            #temp files to use moviepy to check video duration and create thumbnail
             temp = tempfile.NamedTemporaryFile(delete=False)
+            temp_thumb = tempfile.NamedTemporaryFile(delete=False, suffix=".jpg")
+            upload_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             uploaded_file.save(temp)
-            with VideoFileClip(temp.name, audio=False) as video:
+            with VideoFileClip(temp.name) as video:
+                thumbnail = video.get_frame(1)
+                imageio.imwrite(temp_thumb.name, thumbnail)
                 duration = video.duration
             if duration <= 60:
                 video_filename = secure_filename(request.form['title'])
@@ -38,10 +43,13 @@ def upload():
                 with open(temp.name, 'rb') as f:
                     file_contents = f.read()
                 s3.upload_fileobj(io.BytesIO(file_contents), bucket, uname+"/"+video_filename, ExtraArgs={'ACL': 'public-read', 'Metadata': metadata})
+                s3.upload_file(temp_thumb.name, bucket, "thumbnail/"+uname+"/"+video_filename, ExtraArgs={'ACL': 'public-read', 'Metadata': {'time': upload_datetime}})
                 temp.close()
-                os.unlink(temp.name)
+                temp_thumb.close()
                 return jsonify({'success': True, 'message': 'Video uploaded successfully'}), 200
             else:
+                temp.close()
+                temp_thumb.close()
                 return jsonify({'success':False,'message': 'Video too long'}), 500
     except Exception as e:
         print(e)
@@ -60,6 +68,20 @@ def videos():
     except Exception as e:
         print(e)
         return jsonify({'success':False,'message': 'Error fetching videos'}), 500
+    
+@app.route('/thumbnails', methods=['GET'])
+def thumbnails():
+    try:
+        response = s3.list_objects(Bucket="ss-p2", Prefix="thumbnail/")
+        thumbnails = []
+        for obj in response.get('Contents', []):
+            print('Object Key:', obj['Key'])
+            thumbnail = s3.generate_presigned_url('get_object', Params={'Bucket': bucket, 'Key': obj['Key']})
+            thumbnails.append(thumbnail)
+        return jsonify({'success':True,'thumbnails': thumbnails})
+    except Exception as e:
+        print(e)
+        return jsonify({'success':False,'message': 'Error fetching thuumbnails'}), 500
     
 @app.route('/my_videos', methods=['POST'])
 def user_videos():
